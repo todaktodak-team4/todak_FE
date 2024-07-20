59c1710cf197e8e320ca24038058317f16b28ddf
yoon <sooin0105@gmail.com>
2024-07-20 14:41:02 +0900

    충돌 해결

--- todak/src/App.js
+++ todak/src/App.js
@@ -1,11 +1,34 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import * as A from "./css/StyledApp";
 import PlantTreeStepOne from "./pages/PlantTree_stepOne";
 import PlantTreeStepTwo from "./pages/PlantTree_stepTwo";
 import PlantCompleteModal from "./pages/PlantCompleteModal";
 function App() {
   return (
     <BrowserRouter>
+      <A.Header>
+        <A.Logo>
+          <a href="#">
+            <img
+              id="Logo"
+              src={`${process.env.PUBLIC_URL}/img/TodakLogo2.svg`}
+              alt="Logo"
+            />
+          </a>
+          <a href="#">
+            <p>토닥토닥</p>
+          </a>
+        </A.Logo>
+        <A.Privacy>
+          <div>MY</div>
+          <div>회원가입</div>
+          <div>로그인</div>
+        </A.Privacy>
+      </A.Header>
+      <A.Footer></A.Footer>
       <Routes>
+        <Route path="/main" element={<Main />} />
         <Route path="/plantTreeStepOne" element={<PlantTreeStepOne />} />
         <Route path="/plantTreeStepTwo" element={<PlantTreeStepTwo />} />
         <Route path="/completeModal" element={<PlantCompleteModal />} />
