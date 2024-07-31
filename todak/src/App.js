3ba75099c4e5904657866a2ac8f799c2ed1f2a86
sooyeon <limsoo0816@naver.com>
2024-07-31 11:51:44 +0900

    footer fix

--- todak/src/App.js
+++ todak/src/App.js
@@ -72,7 +72,43 @@ function App() {
         <Route path="/layFlower" element={<LayFlower />} />
       </Routes>
 
-      <A.Footer></A.Footer>
+      <A.Footer>
+        <A.Footer1>
+          <img
+            id="Logo"
+            src={`${process.env.PUBLIC_URL}/img/TodakLogo3.svg`}
+            alt="Logo"
+          />
+          <img
+            id="LogoMessage"
+            src={`${process.env.PUBLIC_URL}/img/LogoMessage2.svg`}
+            alt="LogoMessage"
+          />
+          <p id="ment11">
+            <img id="img" src={`${process.env.PUBLIC_URL}/img/footer1.svg`} />
+            서울특별시 성북구 화랑로13길 60
+            <br />
+            60 Hwarang-ro 13-gil, Seongbuk-gu, Seoul
+          </p>
+          <p id="ment13">
+            <img id="img" src={`${process.env.PUBLIC_URL}/img/footer2.svg`} />
+            멋쟁이사자처럼 동덕여자대학교 12기 팀 우리사이
+          </p>
+        </A.Footer1>
+        <A.Footer2>
+          <p id="ment21">당장 도움이 필요하신가요?</p>
+          <p id="ment22">
+            삶을 포기하고 싶다는 생각이 반복적으로 들거나 정신건강 질환의
+            <br />
+            고통이 심할 경우 즉각적으로 전문기관에 도움을 요청하세요.
+          </p>
+          <p id="ment23">
+            보건복지부 자살 예방 상담 전화: 109
+            <br />
+            정신건강 위기 상담 전화: 1577-0199
+          </p>
+        </A.Footer2>
+      </A.Footer>
     </BrowserRouter>
   );
 }
--- todak/src/css/StyledApp.jsx
+++ todak/src/css/StyledApp.jsx
@@ -58,7 +58,63 @@ export const Privacy = styled.div`
   }
 `;
 export const Footer = styled.div`
-  height: 300px;
+  display: flex;
+  flex-direction: row;
+  height: 40vh;
   flex-shrink: 0;
   background: #ffda57;
+  gap: 15vw;
+  justify-content: center;
+  align-iltems: center;
+`;
+
+export const Footer1 = styled.div`
+  color: #655004;
+  font-family: "Pretendard Variable";
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem;
+
+  margin-top: 10vh;
+  margin-bottom: 1vh;
+  p {
+    margin: 0.5rem;
+    margin-left: 0;
+  }
+  img {
+    margin-right: 0.3rem;
+  }
+  #Logo {
+    width: 60px;
+    height: 37px;
+    margin-top: 14px;
+    margin-right: 15px;
+  }
+  #LogoMessage {
+    margin-top: 15px;
+    height: 30px;
+  }
+`;
+export const Footer2 = styled.div`
+  color: #655004;
+  font-family: "Pretendard Variable";
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem;
+
+  margin-top: 5vh;
+
+  #ment21 {
+    color: #655004;
+    font-family: "Pretendard Variable";
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  #ment22 {
+    margin-bottom: 2rem;
+  }
 `;
