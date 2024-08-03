d55a507d98984d578156ff09180e2d61bf42b868
yoon <sooin0105@gmail.com>
2024-08-03 21:24:04 +0900

    style: deliveyInfo css 수정, line 추가

--- todak/src/pages/DeliveryInfo.jsx
+++ todak/src/pages/DeliveryInfo.jsx
@@ -4,6 +4,7 @@ import PopupDom from "./PopupDom";
 import PopupPostCode from "./PopupPostCode";
 import { useNavigate, useLocation } from "react-router-dom";
 import CheckoutPage from "./CheckoutPage";
+import Nav from "./Nav";
 
 function DeliveryInfo() {
   const navigate = useNavigate();
@@ -45,153 +46,161 @@ function DeliveryInfo() {
   const { totalAmount, selectedItems } = location.state || {};
 
   return (
-    <div
-      className={styles.container}
-      style={{
-        backgroundImage: `url("/img/plantTree-bg.png")`,
-        backgroundRepeat: "no-repeat",
-      }}
-    >
+    <>
+      <Nav></Nav>
       <div
-        className={styles.deliWp}
+        className={styles.container}
         style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
+          backgroundImage: `url("/img/plantTree-bg.png")`,
+          backgroundRepeat: "no-repeat",
         }}
       >
-        <div className={styles.top}>
-          <div className={styles.boxImg}>
-            <img src="/img/deliBox.png" alt="tree icon" />
-          </div>
-          <div className={styles.deliTitle}>기억 나무 배송</div>
-        </div>
-
         <div
+          className={styles.deliWp}
           style={{
-            marginLeft: "auto",
-            marginRight: "auto",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          <div className={styles.orderWp}>
-            <img src="/img/d_1.png" alt="1" />
-            <div className={`${styles.orderer} ${styles.title}`}>주문자</div>
-            <input
-              type="text"
-              className={styles.inputBox}
-              placeholder="주문자 성함"
-              value={ordererName}
-              onChange={(e) => setOrdererName(e.target.value)}
-            />
+          <div className={styles.top}>
+            <div className={styles.boxImg}>
+              <img src="/img/deliBox.png" alt="tree icon" />
+            </div>
+            <div className={styles.deliTitle}>기억 나무 배송</div>
           </div>
-          <div className={styles.recipientWp}>
-            <img src="/img/d_2.png" alt="2" />
-            <div className={`${styles.recipient} ${styles.title}`}>수령인</div>
-            <input
-              type="text"
-              className={styles.inputBox}
-              placeholder="수령인 성함"
-              value={recipientName}
-              onChange={(e) => setRecipientName(e.target.value)}
-            />
-          </div>
-          <div className={styles.telWp}>
-            <img src="/img/d_3.png" alt="3" />
-            <div className={`${styles.tel} ${styles.title}`}>전화번호</div>
-            <input
-              type="text"
-              className={styles.inputBox}
-              placeholder="010-0000-0000"
-              value={phoneNumber}
-              onChange={handlePhoneNumberChange}
-              maxLength="13"
-            />
-          </div>
-          <div className={styles.addressWp}>
-            <img src="/img/d_4.png" alt="4" />
-            <div className={`${styles.address} ${styles.title}`}>배송 주소</div>
-            <div className={styles.addressInputWp}>
-              <div className={styles.row}>
-                <input
-                  type="text"
-                  className={`${styles.inputBox} ${styles.zonecode}`}
-                  placeholder="우편번호"
-                  value={zoneCode}
-                  readOnly
-                />
-                <div className={styles.postalCodeBtn} onClick={openPostCode}>
-                  우편번호 찾기
-                </div>
-                <div id="popupDom">
-                  {isPopupOpen && (
-                    <PopupDom>
-                      <PopupPostCode
-                        onClose={closePostCode}
-                        onSelect={handlePostCodeSelection}
-                      />
-                    </PopupDom>
-                  )}
-                </div>
+
+          <div
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <img src="/img/line.png" alt="line" className={styles.line} />
+            <div className={styles.orderWp}>
+              <img src="/img/d_1.png" alt="1" />
+              <div className={`${styles.orderer} ${styles.title}`}>주문자</div>
+              <input
+                type="text"
+                className={styles.inputBox}
+                placeholder="주문자 성함"
+                value={ordererName}
+                onChange={(e) => setOrdererName(e.target.value)}
+              />
+            </div>
+            <div className={styles.recipientWp}>
+              <img src="/img/d_2.png" alt="2" />
+              <div className={`${styles.recipient} ${styles.title}`}>
+                수령인
               </div>
-              <div className={styles.row}>
-                <input
-                  type="text"
-                  className={`${styles.inputBox} ${styles.postalAddress}`}
-                  placeholder="주소"
-                  value={postalAddress}
-                  readOnly
-                />
+              <input
+                type="text"
+                className={styles.inputBox}
+                placeholder="수령인 성함"
+                value={recipientName}
+                onChange={(e) => setRecipientName(e.target.value)}
+              />
+            </div>
+            <div className={styles.telWp}>
+              <img src="/img/d_3.png" alt="3" />
+              <div className={`${styles.tel} ${styles.title}`}>전화번호</div>
+              <input
+                type="text"
+                className={styles.inputBox}
+                placeholder="010-0000-0000"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                maxLength="13"
+              />
+            </div>
+            <div className={styles.addressWp}>
+              <img src="/img/d_4.png" alt="4" />
+              <div className={`${styles.address} ${styles.title}`}>
+                <span className={styles.adt}>배송 주소</span>
               </div>
-              <div className={styles.row}>
-                <input
-                  type="text"
-                  className={styles.inputBox}
-                  placeholder="상세 주소 입력"
-                  value={detailAddress}
-                  onChange={(e) => setDetailAddress(e.target.value)}
-                />
+              <div className={styles.addressInputWp}>
+                <div className={styles.row}>
+                  <input
+                    type="text"
+                    className={`${styles.inputBox} ${styles.zonecode}`}
+                    placeholder="우편번호"
+                    value={zoneCode}
+                    readOnly
+                  />
+                  <div className={styles.postalCodeBtn} onClick={openPostCode}>
+                    우편번호 찾기
+                  </div>
+                  <div id="popupDom">
+                    {isPopupOpen && (
+                      <PopupDom>
+                        <PopupPostCode
+                          onClose={closePostCode}
+                          onSelect={handlePostCodeSelection}
+                        />
+                      </PopupDom>
+                    )}
+                  </div>
+                </div>
+                <div className={styles.row}>
+                  <input
+                    type="text"
+                    className={`${styles.inputBox} ${styles.postalAddress}`}
+                    placeholder="주소"
+                    value={postalAddress}
+                    readOnly
+                  />
+                </div>
+                <div className={styles.row}>
+                  <input
+                    type="text"
+                    className={styles.inputBox}
+                    placeholder="상세 주소 입력"
+                    value={detailAddress}
+                    onChange={(e) => setDetailAddress(e.target.value)}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "",
-            height: "100%",
-          }}
-        >
           <div
-            className={styles.backBtn}
-            onClick={() => navigate("/deliveryProduct")}
-            style={{ marginLeft: "10%" }}
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "",
+              height: "100%",
+            }}
           >
-            이전
+            <div
+              className={styles.backBtn}
+              onClick={() => navigate("/deliveryProduct")}
+              style={{ marginLeft: "10%" }}
+            >
+              이전
+            </div>
+            <div style={{ flex: 3 }}></div>
+            <div
+              className={styles.deliBtn}
+              onClick={openPaymentModal}
+              style={{ marginRight: "5%" }}
+            >
+              결제 및 배송 신청
+            </div>
           </div>
-          <div style={{ flex: 3 }}></div>
-          <div
-            className={styles.deliBtn}
-            onClick={openPaymentModal}
-            style={{ marginRight: "5%" }}
-          >
-            결제 및 배송 신청
-          </div>
+          {isPaymentModalOpen && (
+            <CheckoutPage
+              totalAmount={totalAmount}
+              selectedItems={selectedItems}
+              ordererName={ordererName}
+              recipientName={recipientName}
+              phoneNumber={phoneNumber}
+              postalAddress={postalAddress}
+              detailAddress={detailAddress}
+              onClose={closePaymentModal} // Pass the close function
+            />
+          )}
         </div>
-        {isPaymentModalOpen && (
-          <CheckoutPage
-            totalAmount={totalAmount}
-            selectedItems={selectedItems}
-            ordererName={ordererName}
-            recipientName={recipientName}
-            phoneNumber={phoneNumber}
-            postalAddress={postalAddress}
-            detailAddress={detailAddress}
-            onClose={closePaymentModal} // Pass the close function
-          />
-        )}
       </div>
-    </div>
+    </>
   );
 }
 
