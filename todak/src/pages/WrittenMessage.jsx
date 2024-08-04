de8043c55b84392c73748ab594478f38d1ec0d92
yoon <sooin0105@gmail.com>
2024-08-05 05:13:43 +0900

    feat: 마이페이지 헌화내역/추모글 내역 완료

--- todak/src/pages/WrittenMessage.jsx
+++ todak/src/pages/WrittenMessage.jsx
@@ -4,15 +4,19 @@ import styles from "../css/StyledWrittenMessage.module.css";
 
 function WrittenMessage() {
   const [messages, setMessages] = useState([]);
+  const [hoveredId, setHoveredId] = useState(null); // New state for hovered message ID
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:8000/message/my-messages/", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-        });
+        const response = await axios.get(
+          "http://127.0.0.1:8000/message/my-messages/",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+          }
+        );
         setMessages(response.data);
       } catch (error) {
         console.error("Error fetching messages:", error);
@@ -31,12 +35,26 @@ function WrittenMessage() {
         className={styles.containerBg}
       />
       <div className={styles.logo}>
-        <img src="/img/writtenMessage.png" alt="마이페이지 로고" />
+        <img src="/img/writtenMessage.png" alt="로고" />
+      </div>
+      <div className={styles.latest}>
+        최신순
+        <span
+          style={{
+            fontSize: "23px",
+            position: "relative",
+            left: "15px",
+            fontWeight: "400",
+          }}
+        >
+          *글에 마우스를 가져다대면 작성한 추모글의 전체 내용을 볼 수 있습니다.
+        </span>
       </div>
       <div className={styles.contentContainer}>
-        <div className={styles.latest}>최신순</div>
         {messages.length === 0 ? (
-          <p>추모글이 없습니다.</p>
+          <p style={{ fontSize: "40px", fontFamily: "Pretendard Variable" }}>
+            아직 남긴 추모글이 없습니다.
+          </p>
         ) : (
           messages.map((message) => (
             <div key={message.id} className={styles.contentList}>
@@ -50,9 +68,32 @@ function WrittenMessage() {
                   top: "20px",
                 }}
               />
+              <img
+                src="/img/writtenline.png"
+                alt="점"
+                style={{
+                  width: "2px",
+                  height: "157px",
+                  position: "relative",
+                  top: "43px",
+                  right: "8px",
+                }}
+              />
               <div className={styles.memorialHall}>{message.hallName}</div>
               <div className={styles.contentWp}>
-                <div className={styles.content}>{message.content}</div>
+                <div
+                  className={`${
+                    hoveredId === null
+                      ? styles.content
+                      : hoveredId === message.id
+                      ? styles.thisContent
+                      : styles.hovered
+                  }`}
+                  onMouseEnter={() => setHoveredId(message.id)}
+                  onMouseLeave={() => setHoveredId(null)}
+                >
+                  {message.content}
+                </div>
               </div>
             </div>
           ))
