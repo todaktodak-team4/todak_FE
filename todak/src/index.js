e9408ade3daad25e2a5f8acb059fb173e761e56e
Yerim <yerim0307@gmail.com>
2024-08-06 18:57:50 +0900

    resolve app.js import HashRouter error

--- todak/src/index.js
+++ todak/src/index.js
@@ -1,6 +1,7 @@
 // index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const container = document.getElementById("root");
